f6c97a7b2f837ef2db740ca1f7eb586dc1ec32c7
Jacob Douthett <jadouth@gmail.com>
2022-09-28 20:32:40 -0400

    All tables and graphs completed

--- static/js/app.js
+++ static/js/app.js
@@ -2,6 +2,8 @@
 // Global variable to use to get data
 const url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json"
 
+
+/// -- Creating the bar graph -- ///
 function barGraph(sampleID)
 {
     console.log(`show barGraph(${sampleID})`);
@@ -15,10 +17,9 @@ function barGraph(sampleID)
         let otu_labels = result.otu_labels;
         let sample_values = result.sample_values;
 
-        //create a bar graph
-        //create a trace object
         //Create the yticks for the graph by using map() to grab the otu_ids in the array
         let yticks = otu_ids.slice(0, 10).map(otu_id => `OTU ${otu_id}`).reverse();
+        //create a trace object
         let barData = {
             x: sample_values.slice(0, 10).reverse(),
             y: yticks,
@@ -38,7 +39,10 @@ function barGraph(sampleID)
         Plotly.newPlot("bar", barArray, barLayout)
     });
 }
+// ** Code provided by Instructor Dom from Homework 14 tutorial ** //
 
+
+/// -- Creating the bubble graph -- ///
 function bubbleGraph(sampleID)
 {
     console.log(`show bubbleGraph(${sampleID})`);
@@ -79,10 +83,39 @@ function bubbleGraph(sampleID)
          Plotly.newPlot("bubble", bubbleArray, bubbleLayout)
     });
 }
+// ** Code provided by Instructor Dom from Homework 14 tutorial ** //
 
+
+/// -- Create metadata/demographic table -- ///
 function metaData(sampleID)
 {
     console.log(`show metaData(${sampleID})`);
+
+    let selector = d3.select("#sample-metadata")
+
+    d3.json(url).then(data => {
+        let metadata = data.metadata;
+        // for (let i = 0; i < metadata.length; i++){
+        //     let tabledata = metadata[i];
+        //     selector.append('option').text(tabledata);
+        // };
+
+        let resultsArray = metadata.filter(m => m.id == sampleID);
+        let result = resultsArray[0];
+        console.log(result);
+
+
+        let id = result.id;
+        let ethnicity = result.ethnicity;
+        let gender = result.gender;
+        let age = result.age;
+        let location = result.location;
+        let bbtype = result.bbtype;
+        let wfreq = result.wfreq;
+
+        metaDemoInfo = `ID: ${id} <br> Ethnicity: ${ethnicity} <br> Gender: ${gender} <br> Age: ${age} <br> Location: ${location} <br> BBType: ${bbtype} <br> WFreq: ${wfreq}`
+        document.getElementById('sample-metadata').innerHTML = metaDemoInfo;
+    });
 }
 
 function optionChanged(sampleID)
@@ -92,6 +125,7 @@ function optionChanged(sampleID)
     bubbleGraph(sampleID);
     metaData(sampleID);
 }
+// ** Code provided by Instructor Dom from Homework 14 tutorial ** //
 
 function fillDropdown()
 {
@@ -123,7 +157,9 @@ function fillDropdown()
     //Show meta/demographic data
     metaData(initialID);
     });
-    // Code provided by Instructor Dom from Homework 14 tutorial
+
+
+    // ** Code provided by Instructor Dom from Homework 14 tutorial ** //
 }
 
 fillDropdown();
